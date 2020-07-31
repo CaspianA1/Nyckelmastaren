b152f5e116b07f3835965e9b7fdae4461cedbb9e
Your Name <you@example.com>
2020-07-31 16:00:44 -0400

    Add dependency files

---
+++ dependencies_1.sh
@@ -0,0 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install python
+brew install git
+
+pip3 install simpleaudio || pip install simpleaudio
+pip3 install pillow || pip install pillow
