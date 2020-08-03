a6c3ecd2006272c09c7c7493d8b4115c71c8ff5c
Your Name <you@example.com>
2020-08-03 11:00:19 -0400

    Update dependencies for linux

--- dependencies_1.sh
+++ dependencies_1.sh
@@ -2,5 +2,6 @@
 brew install python
 brew install git
 
+sudo apt-get install libasound2-dev  # for linux machines
 pip3 install simpleaudio || pip install simpleaudio
 pip3 install pillow || pip install pillow
